45dd59785a870d10d3957f0100d0412e6a694ba5
Retro131 <geoorgg79@gmail.com>
2024-12-08 02:35:33 +0700

    добавил класс для спрайтов

---
+++ Sokoban/Sokoban/Sprites/Sprite.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sokoban
+{
+    public class Sprite
+    {
+        public Texture2D _texture { get; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Color Color { get; set; }
+        public float speed { get; set; }
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+            }
+        }
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        public bool IsTouchingLeft(Sprite sprite)
+        {
+            return this.Rectangle.Right + this.Velocity.X > sprite.Rectangle.Left &&
+                this.Rectangle.Left < sprite.Rectangle.Left &&
+                this.Rectangle.Bottom > sprite.Rectangle.Top &&
+                this.Rectangle.Top < sprite.Rectangle.Bottom;
+        }
+        public bool IsTouchingRight(Sprite sprite)
+        {
+            return this.Rectangle.Left + this.Velocity.X < sprite.Rectangle.Right &&
+                this.Rectangle.Right > sprite.Rectangle.Right &&
+                this.Rectangle.Bottom > sprite.Rectangle.Top &&
+                this.Rectangle.Top < sprite.Rectangle.Bottom;
+        }
+        public bool IsTouchingTop(Sprite sprite)
+        {
+            return this.Rectangle.Bottom + this.Velocity.Y > sprite.Rectangle.Top &&
+                this.Rectangle.Top < sprite.Rectangle.Top &&
+                this.Rectangle.Right > sprite.Rectangle.Left &&
+                this.Rectangle.Left < sprite.Rectangle.Right;
+        }
+        public bool IsTouchingBottom(Sprite sprite)
+        {
+            return this.Rectangle.Top + this.Velocity.Y < sprite.Rectangle.Bottom &&
+                this.Rectangle.Bottom > sprite.Rectangle.Bottom &&
+                this.Rectangle.Right > sprite.Rectangle.Left &&
+                this.Rectangle.Left < sprite.Rectangle.Right;
+        }
+    }
+}
