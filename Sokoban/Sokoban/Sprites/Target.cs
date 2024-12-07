d41c50dd199adf062114abe138b91fb72f951109
Retro131 <geoorgg79@gmail.com>
2024-12-08 02:55:16 +0700

    Добавил класс Target

---
+++ Sokoban/Sokoban/Sprites/Target.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    public class Target : Sprite
+    {
+        public Target(Texture2D texture) : base(texture) { }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            base.Update(gameTime, sprites);
+        }
+    }
+}
