aff5da93f1e9cc88491a7178c7ee05792022832d
Retro131 <geoorgg79@gmail.com>
2024-12-08 02:54:20 +0700

    Добавил класс Box

---
+++ Sokoban/Sokoban/Sprites/Box.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Sokoban
+{
+    public class Box : Sprite
+    {
+        public Box(Texture2D texture) : base(texture) { }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            base.Update(gameTime, sprites);
+        }
+    }
+}
