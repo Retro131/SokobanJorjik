d1591527d7cc377ec9b54eb49a0f9294d571a167
Retro131 <geoorgg79@gmail.com>
2024-12-08 20:29:23 +0700

    Добавил движения и обработку столкновений для Player

--- Sokoban/Sokoban/Sprites/Player.cs
+++ Sokoban/Sokoban/Sprites/Player.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 
 namespace Sokoban
@@ -13,7 +14,32 @@ namespace Sokoban
         }
         public override void Update(GameTime gameTime, List<Sprite> sprites)
         {
-            base.Update(gameTime, sprites);
+            Move();
+            foreach (var sprite in sprites)
+            {
+                if (sprite == this)
+                    continue;
+                if(Velocity.X > 0 && IsTouchingLeft(sprite) ||
+                        Velocity.X < 0 && IsTouchingRight(sprite))
+                    Velocity.X = 0;
+                if(Velocity.Y >  0 && IsTouchingTop(sprite) ||
+                        Velocity.Y < 0 && IsTouchingBottom(sprite))
+                    Velocity.Y = 0;
+            }
+            Position += Velocity;
+            Velocity = Vector2.Zero;
+        }
+
+        public void Move()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                Velocity.X -= speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+                Velocity.X += speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.W))
+                Velocity.Y -= speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+                Velocity.Y += speed;
         }
         public override float GetPriority() => 0.1f;
         public override string ToString() => "Player.png";
