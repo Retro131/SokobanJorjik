5d39c2fcc4eed69f8116b39c91d429b897c10288
Retro131 <geoorgg79@gmail.com>
2024-12-10 04:26:29 +0700

    Добавил небольшую реализацию для класса State

--- Sokoban/Sokoban/States/State.cs
+++ Sokoban/Sokoban/States/State.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using System.Net;
 
 namespace Sokoban
 {
-   public class State
+    public class State
     {
+        protected ContentManager _contentManager;
+        protected GraphicsDevice _graphics;
+        protected Game1 _game;
+
+        public State(Game1 game, ContentManager contentManager, GraphicsDevice graphics)
+        {
+            _contentManager = contentManager;
+            _graphics = graphics;
+            _game = game;
+        }
+        public virtual void Draw(GameTime gameTime,SpriteBatch spriteBatch) { }
+        public virtual void Update(GameTime gameTime) { }
     }
 }
