e4f06aee6573b36a2f64bbb8e5d3a2277c3eb01a
Retro131 <geoorgg79@gmail.com>
2024-12-12 04:05:57 +0700

    Добавил Синглтон для имени пользователя

---
+++ Sokoban/Sokoban/UserName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    public class UserName
+    {
+        private static UserName instance;
+
+        private UserName()
+        { }
+
+        public static UserName getInstance()
+        {
+            if (instance == null)
+                instance = new UserName();
+            return instance;
+        }
+    }
+}
